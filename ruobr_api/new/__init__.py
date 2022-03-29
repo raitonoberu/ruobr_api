175ddac4e5c47cb1e00640cdd66c2586bdc32df2
Denis <raitonoberu@mail.ru>
2022-03-29 23:35:50 +0700

    Update __init__.py

--- ruobr_api/new/__init__.py
+++ ruobr_api/new/__init__.py
@@ -9,7 +9,7 @@ api3d.ruobr.ru
    >>> from ruobr_api.new import Ruobr
    >>> r = Ruobr('username', 'password')
    >>> r.getUser()
-   User(id=7694824, user_id=15543421, status='child', first_name='Иван', last_name='Иванов', middle_name='Иванович', birth_date=datetime.date(2004, 10, 10), user_img='https://ruobr.ru/mediac/avatars/123.JPEG', school='69-МБОУ "СОШ №69"', school_terrirtory_id=11, school_is_tourniquet=False, readonly=False, school_is_food=5, group='10А', gps_tracker=False)
+   User(id=7694824, user_id=15543421, status='child', first_name='Иван', last_name='Иванов', middle_name='Иванович', birth_date=datetime.date(2004, 10, 10), user_img='https://ruobr.ru/mediac/avatars/123.JPEG', school='69-МБОУ "СОШ №69"', school_terrirtory_id=11, school_is_tourniquet=0, readonly=False, school_is_food=5, group='10А', gps_tracker=False)
 
 :authors: raitonoberu
 :license: Apache License, Version 2.0, see LICENSE file
